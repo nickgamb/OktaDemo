10cf4a48f01fa06810a676dc47e491956fa98c28
nickgamb <nickgamb@gmail.com>
2019-04-01 14:23:40 -0600

    Initial commit

---
+++ OktaDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OktaDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ OktaDemo/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Okta.AspNet;
+using Owin;
+
+[assembly: OwinStartup(typeof(OktaDemo.Startup))]
+
+namespace OktaDemo
+{
+    public class Startup
+    {
+        //To use Okta hosted Login, use this confit, and uncomment login and profile elements in layout.
+        /*public void Configuration(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOktaMvc(new OktaMvcOptions()
+            {
+                OktaDomain = ConfigurationManager.AppSettings["okta:OktaDomain"],
+                ClientId = ConfigurationManager.AppSettings["okta:ClientId"],
+                ClientSecret = ConfigurationManager.AppSettings["okta:ClientSecret"],
+                RedirectUri = ConfigurationManager.AppSettings["okta:RedirectUri"],
+                PostLogoutRedirectUri = ConfigurationManager.AppSettings["okta:PostLogoutRedirectUri"],
+                GetClaimsFromUserInfoEndpoint = true,
+                Scope = new List<string> { "openid", "profile", "email" },
+            });
+        }*/
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseOktaWebApi(new OktaWebApiOptions()
+            {
+                OktaDomain = ConfigurationManager.AppSettings["okta:OktaDomain"],
+            });
+        }
+
+    }
+}
